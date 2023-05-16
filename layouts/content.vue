0df7586d3ecac80ce52d1f6857831061bbd4463e
Tito Riskimianto <riskimianto@gmail.com>
2023-05-16 10:18:03 +0700

    Initial Commit

---
+++ layouts/content.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        <section class="bg-pink-100">
+            <div class="py-8 mx-8 sm:mx-12 md:mx-24 lg:mx-40 xl:mx-60">
+                <div class="bg-white rounded-lg p-8">
+                    <slot />
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
---
+++ layouts/footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="bg-pink-300 py-4 text-violet-900 text-center">
+        <p>&copy; 2023 Cooking Recipes</p>
+    </footer>
+</template>
