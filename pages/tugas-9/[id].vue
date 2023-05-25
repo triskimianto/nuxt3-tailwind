f7e08efd194dd6b29f23417e85560bf50cd04019
Tito Riskimianto <riskimianto@gmail.com>
2023-05-25 10:23:39 +0700

    updated tugas-9

--- pages/tugas-9/[id].vue
+++ pages/tugas-9/[id].vue
@@ -1,28 +1,28 @@
 <template>
     <NuxtLayout :name="content">
         
-    <div class="flex px-2 py-4" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-1 md:space-x-3">
-            <li class="inline-flex items-center">
-                <NuxtLink to="/" class="inline-flex items-center text-sm font-medium text-violet-500 hover:text-violet-700">
-                    <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-                    Home
-                </NuxtLink>
-            </li>
-            <li>
-                <div class="flex items-center">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                    <NuxtLink to="/tugas-9/" class="ml-1 text-sm font-medium text-violet-500 hover:text-violet-700 md:ml-2">Produk</NuxtLink>
-                </div>
-            </li>
-            <li aria-current="page">
-                <div class="flex items-center">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                    <span class="ml-1 text-sm font-medium text-gray-700 md:ml-2">{{ produk?.nama }}</span>
-                </div>
-            </li>
-        </ol>
-    </div>
+        <div class="flex px-2 py-4" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                <li class="inline-flex items-center">
+                    <NuxtLink to="/" class="inline-flex items-center text-sm font-medium text-violet-500 hover:text-violet-700">
+                        <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                        Home
+                    </NuxtLink>
+                </li>
+                <li>
+                    <div class="flex items-center">
+                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        <NuxtLink to="/tugas-9/" class="ml-1 text-sm font-medium text-violet-500 hover:text-violet-700 md:ml-2">Produk</NuxtLink>
+                    </div>
+                </li>
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        <span class="ml-1 text-sm font-medium text-gray-700 md:ml-2">{{ produk?.nama }}</span>
+                    </div>
+                </li>
+            </ol>
+        </div>
 
         <div class="flex flex-col md:flex-row">
             <div class="w-full md:w-1/2 px-2">
@@ -53,7 +53,7 @@
             <h1 class="text-xl font-bold px-2">Ulasan</h1>
             <div class="flex flex-col md:flex-row mt-2">
                 <div class="w-full md:w-1/3 px-2">
-                    <h1>&#11088; <span class="text-6xl">{{ produk?.detail.rating }}</span><span class="text-gray-500">/5.0</span></h1>
+                    <h1>&#11088; <span class="text-6xl">{{ produk?.detail.rating }}</span><span class="text-gray-500">/5</span></h1>
                 </div>
                 <div class="w-full md:w-2/3 px-2">
                     <div v-for="u in produk?.detail.ulasan" :key="u.nama" class="w-full py-2">
--- pages/tugas-9/index.vue
+++ pages/tugas-9/index.vue
@@ -1,14 +1,27 @@
 <template>
     <NuxtLayout :name="content">
+        <div class="flex px-2 py-4" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                <li class="inline-flex items-center">
+                    <NuxtLink to="/" class="inline-flex items-center text-sm font-medium text-violet-500 hover:text-violet-700">
+                        <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                        Home
+                    </NuxtLink>
+                </li>
+                <li>
+                    <div class="flex items-center">
+                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        <span class="ml-1 text-sm font-medium text-gray-700 md:ml-2">Produk</span>
+                    </div>
+                </li>
+            </ol>
+        </div>        
         <div class="max-w-sm w-full rounded-lg bg-white mb-8 lg:max-w-full lg:flex">                
-            <div class="w-full px-6 py-4">
-                <div class="flex flex-col md:flex-row">
-                    <NuxtLink to="/" class="w-1/4 text-xl hover:text-pink-700">Kembali</NuxtLink>
-                    <h1 class="w-2/3 text-xl">
-                        Tugas 9 - 
-                        <span class="font-bold text-xl text-violet-700">Loop dan Menampilkan JSON</span>        
-                    </h1>
-                </div>                
+            <div class="w-full px-6 py-4">             
+                <h1 class="text-center text-xl">
+                    Tugas 9 - 
+                    <span class="font-bold text-xl text-violet-700">Loop dan Menampilkan JSON</span>        
+                </h1>                
             </div>
         </div>
         <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
